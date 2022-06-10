446b8a6acb1168aadbc745c44c77e0899c65d4b5
Nagaraju Nallangi <nagaraju.nallangi@tietoevry.com>
2022-06-10 09:26:02 +0000

    adding research solution
    git-tfs-id: [https://dev.azure.com/nnallangi]$/tfvc/research;C4

---
+++ Web-Application-2/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Application_2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // this is Application 2
+            lakeside.classlib1.Class1 CS1 = new lakeside.classlib1.Class1();
+            lakeside.classlib2.Class1 cs2 = new lakeside.classlib2.Class1();
+
+        }
+    }
+}
---
+++ Web-Application-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_Application_2.Global" Language="C#" %>
